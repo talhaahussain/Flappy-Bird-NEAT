6a7670cbb53f7794bfd796d7236f627569a1b487
talhaahussain <talhaahussain@gmail.com>
2023-07-04 16:11:11 +0100

    Modified main function in flappy_bird.py to work with neat
    Modified main function to work as fitness function.
    Added new lists of neural networks and genomes.
    Added punishment for poorly performing birds (elimination/decreased
    fitness).
    Added reward for successful birds (increased fitness)

--- flappy_bird.py
+++ flappy_bird.py
@@ -166,8 +166,19 @@ def draw_window(win, bird, pipes, base, score):
     bird.draw(win)
     pygame.display.update()
 
-def main():
-    bird = Bird(100, 300)
+def main(genomes, config):
+    nets = []
+    ge = []
+    birds = []
+
+    for g in genomes:
+        net = neat.nn.FeedForwardNetwork(g, config)
+        nets.append(net)
+        birds.append(Bird(200, 350))
+        g.fitness = 0
+        ge.append(g)
+
+
     base = Base(600)
     pipes = [Pipe(700)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -184,27 +195,37 @@ def main():
         add_pipe = False
         rem = []
         for pipe in pipes:
-            if pipe.collide(bird):
-                pass
-
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x] -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+                    
+ 
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
+            
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
                 rem.append(pipe)
-
-            if not pipe.passed and pipe.x < bird.x:
-                pipe.passed = True
-                add_pipe = True
-
+            
             pipe.move()
 
         if add_pipe:
             score += 1
+            for g in ge:
+                g.fitness += 5
             pipes.append(Pipe(500))
 
         for r in rem:
             pipes.remove(r)
 
-        if bird.y + bird.img.get_height() >= 730:
-            pass
+        for x, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() >= 730:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
 
         base.move()
         draw_window(win, bird, pipes, base, score)
@@ -218,7 +239,11 @@ def run(config_path):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
     p = neat.Population(config)
 
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
 
+    winner = p.run(main, 50)
 
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
