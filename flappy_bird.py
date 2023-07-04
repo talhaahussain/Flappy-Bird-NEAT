c9d9be2b9c8b2f132f3a00f198095e6b91ee469c
talhaahussain <talhaahussain@gmail.com>
2023-07-04 11:30:25 +0100

    Added move, draw and collide methods to Pipe class in flappy_bird.py
    Background scaling bug is no longer an issue (was fixed in an earlier
    commit); no known bugs.

--- flappy_bird.py
+++ flappy_bird.py
@@ -80,6 +80,7 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
+
 class Pipe:
     GAP = 200
     VEL = 5
@@ -101,6 +102,30 @@ class Pipe:
         self.height = random.randrange(50, 450)
         self.top = self.height = self.PIPE_TOP.get_height()
         self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.VEL
+
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+    def collide(self, bird, win):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+       
+        if t_point or b_point:
+            return True
+
+        return False
+
 
 def draw_window(win, bird):
     win.blit(BG_IMG, (0, 0))
