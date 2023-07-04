0dbca948a4e2923a1bd6f6d8651982dd7ed3d113
talhaahussain <talhaahussain@gmail.com>
2023-07-04 15:54:47 +0100

    Begin working on NEAT - added config file and began config in flappy_bird.py
    Added run function and imported config file.

--- flappy_bird.py
+++ flappy_bird.py
@@ -214,3 +214,13 @@ def main():
 
 main()
 
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+    p = neat.Population(config)
+
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+    run(config_path)
